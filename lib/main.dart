00a84f599ce89fbdc9fdd22753a9e6c2ac205bd3
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-07-23 22:11:58 +0545

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1b2R0d3p0c3JicXRmaWlzeHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NjM4MzQsImV4cCI6MjA2ODMzOTgzNH0.6LgBKcqa_fzM0czazc5eo6Zkj6FX_H_AftJvIy5i_y8',
   );
+  //Init the location Service
   await LocationService.initialize();
   runApp(const MyApp());
 }
