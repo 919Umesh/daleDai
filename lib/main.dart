1d656848b906160a15880a3907fb952c629c9d5f
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-07-23 21:39:47 +0545

    Init Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:omspos/services/location/location_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'config/config.dart';
 
@@ -9,5 +10,6 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1b2R0d3p0c3JicXRmaWlzeHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NjM4MzQsImV4cCI6MjA2ODMzOTgzNH0.6LgBKcqa_fzM0czazc5eo6Zkj6FX_H_AftJvIy5i_y8',
   );
+  await LocationService.initialize();
   runApp(const MyApp());
 }
--- lib/services/location/location_service.dart
+++ lib/services/location/location_service.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:geolocator/geolocator.dart';
 import 'package:omspos/utils/custom_log.dart';
 
 class LocationService {
