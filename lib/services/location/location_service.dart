3b6033bb255fa34ccf21bcb49d5d730ee294504e
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-07-23 21:40:46 +0545

    CLear

--- lib/services/location/location_service.dart
+++ lib/services/location/location_service.dart
@@ -3,21 +3,18 @@ import 'package:omspos/utils/custom_log.dart';
 
 class LocationService {
   static Position? _currentPosition;
-  
-  // Initialize location services and check permissions
+
   static Future<void> initialize() async {
     await _checkLocationService();
     await _handleLocationPermission();
     await getCurrentLocation();
   }
 
-  // Check if location services are enabled
   static Future<bool> _checkLocationService() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     CustomLog.actionLog(value: "Location service enabled: $serviceEnabled");
-    
+
     if (!serviceEnabled) {
-      // Open location settings if service is disabled
       CustomLog.actionLog(value: "Requesting to enable location services");
       await Geolocator.openLocationSettings();
       return false;
@@ -25,7 +22,6 @@ class LocationService {
     return true;
   }
 
-  // Handle location permissions
   static Future<bool> _handleLocationPermission() async {
     bool serviceEnabled = await _checkLocationService();
     if (!serviceEnabled) return false;
@@ -43,8 +39,6 @@ class LocationService {
 
     if (permission == LocationPermission.deniedForever) {
       CustomLog.actionLog(value: "Location permissions are permanently denied");
-      // You might want to show a dialog explaining why you need permissions
-      // and direct the user to app settings
       return false;
     }
 
@@ -52,22 +46,22 @@ class LocationService {
     return true;
   }
 
-  // Get current location with caching
   static Future<Position?> getCurrentLocation() async {
     try {
       bool hasPermission = await _handleLocationPermission();
       if (!hasPermission) return null;
 
-      // Use cached position if available and not too old (e.g., less than 5 minutes old)
-      if (_currentPosition != null && 
-          DateTime.now().difference(_currentPosition!.timestamp!) < Duration(minutes: 5)) {
+      if (_currentPosition != null &&
+          DateTime.now().difference(_currentPosition!.timestamp!) <
+              Duration(minutes: 5)) {
         return _currentPosition;
       }
 
       _currentPosition = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.best,
       );
-      CustomLog.actionLog(value: "Fetched new location: ${_currentPosition!.toJson()}");
+      CustomLog.actionLog(
+          value: "Fetched new location: ${_currentPosition!.toJson()}");
       return _currentPosition;
     } catch (e) {
       CustomLog.errorLog(value: "Error getting location: $e");
