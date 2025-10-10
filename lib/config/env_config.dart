ab139b43e95b2d0eed4b145fec0d9888380a2301
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-10 23:35:19 +0545

    Client Env Id

--- lib/config/env_config.dart
+++ lib/config/env_config.dart
@@ -1,9 +1,9 @@
-// lib/config/env_config.dart
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EnvConfig {
   static String get supabaseUrl => dotenv.get('SUPABASE_URL');
   static String get supabaseAnonKey => dotenv.get('SUPABASE_ANON_KEY');
+  static String get webClientId => dotenv.get('WEB_CLIENT_ID');
 
   static Future<void> load() async {
     await dotenv.load(fileName: '.env');
