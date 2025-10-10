932ded2162944a768bc4684a5a7122a4947b79ec
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-10 23:47:53 +0545

    Get Location

--- lib/screen/map/api/map_api.dart
+++ lib/screen/map/api/map_api.dart
@@ -1,4 +1,5 @@
 import 'package:omspos/screen/home/model/property_model.dart';
+import 'package:omspos/screen/map/model/location_view_model.dart';
 import 'package:omspos/screen/map/model/map_model.dart';
 import 'package:omspos/services/api/supabase_helper.dart';
 
@@ -6,7 +7,7 @@ class MapLocationApi {
   static Future<List<MapModel>> getAllProperties() async {
     final response = await SupabaseProvider.fetchData(
       tableName: 'properties',
-      limit: 100, 
+      limit: 100,
       cacheFirst: false,
     );
 
@@ -20,6 +21,26 @@ class MapLocationApi {
 
     return (response['data'] as List)
         .map((propertyJson) => MapModel.fromJson(propertyJson))
+        .toList();
+  }
+
+  static Future<List<LocationView>> getLocations() async {
+    final response = await SupabaseProvider.fetchData(
+      tableName: 'location_view',
+      limit: 100,
+      cacheFirst: true,
+    );
+
+    if (response['error'] == true) {
+      throw Exception(response['message'] ?? 'Failed to fetch properties');
+    }
+
+    if (response['data'].isEmpty) {
+      return [];
+    }
+
+    return (response['data'] as List)
+        .map((locationJson) => LocationView.fromJson(locationJson))
         .toList();
   }
 }
