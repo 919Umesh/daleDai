044cfbb2077bc1c0c1928b8b3b71098b4bb9658f
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-10 20:58:06 +0545

    Git Merge

---
+++ lib/services/gitCommands/git_command.dart
@@ -0,0 +1,12 @@
+//The method to bring the changes form the other branches to the main branch
+// # Switch to main branch
+// git checkout main
+
+// # Pull latest changes from remote
+// git pull origin main
+
+// # Merge dev branch into main
+// git merge dev
+
+// # Push to remote
+// git push origin main
