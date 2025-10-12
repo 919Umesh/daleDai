db7febf7e0ed419c4c7707384769ff96fc002d91
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-12 09:59:51 +0545

    Cached Data

--- lib/screen/map/api/map_api.dart
+++ lib/screen/map/api/map_api.dart
@@ -1,33 +1,12 @@
 import 'package:omspos/screen/map/model/location_view_model.dart';
-import 'package:omspos/screen/map/model/map_model.dart';
 import 'package:omspos/services/api/supabase_helper.dart';
 
 class MapLocationApi {
-  static Future<List<MapModel>> getAllProperties() async {
-    final response = await SupabaseProvider.fetchData(
-      tableName: 'properties',
-      limit: 100,
-      cacheFirst: false,
-    );
-
-    if (response['error'] == true) {
-      throw Exception(response['message'] ?? 'Failed to fetch properties');
-    }
-
-    if (response['data'].isEmpty) {
-      return [];
-    }
-
-    return (response['data'] as List)
-        .map((propertyJson) => MapModel.fromJson(propertyJson))
-        .toList();
-  }
-
-  static Future<List<LocationView>> getLocations() async {
+  static Future<List<LocationView>> getLocations(bool? isRefresh) async {
     final response = await SupabaseProvider.fetchData(
       tableName: 'location_view',
       limit: 100,
-      cacheFirst: true,
+      cacheFirst:  isRefresh != null ? !isRefresh : false,
     );
 
     if (response['error'] == true) {
--- lib/screen/map/screen/map_screen.dart
+++ lib/screen/map/screen/map_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:omspos/screen/map/state/map_state.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -20,10 +23,32 @@ class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Properties Map'),
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter:
+            LatLng(51.509364, -0.128928), // Center the map over London
+        initialZoom: 9.2,
       ),
+      children: [
+        TileLayer(
+          // Bring your own tiles
+          urlTemplate:
+              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+          userAgentPackageName:
+              'com.oms.omspos.posoms.omspos', // Add your app identifier
+          // And many more recommended properties!
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse(
+                  'https://openstreetmap.org/copyright')), // (external)
+            ),
+            // Also add images...
+          ],
+        ),
+      ],
     );
   }
 }
