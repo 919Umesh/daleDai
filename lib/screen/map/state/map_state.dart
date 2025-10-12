1a6a83de982f601ded288bc9c78693e2d90ab8fa
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-12 09:42:25 +0545

    Flag Bug Remove

--- lib/screen/map/state/map_state.dart
+++ lib/screen/map/state/map_state.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:omspos/screen/map/api/map_api.dart';
 import 'package:omspos/screen/map/model/location_view_model.dart';
 import 'package:omspos/screen/map/model/map_model.dart';
 import 'package:omspos/services/location/location_service.dart';
+import 'package:omspos/utils/connection_status.dart';
 import 'package:omspos/utils/custom_log.dart';
 
 class MapState extends ChangeNotifier {
@@ -14,7 +16,7 @@ class MapState extends ChangeNotifier {
   BuildContext? get context => _context;
   set getContext(BuildContext value) {
     _context = value;
-    networkSuccess();
+    checkConnection();
   }
 
   bool _isLoading = false;
@@ -41,12 +43,17 @@ class MapState extends ChangeNotifier {
   String? _errorMessage;
   String? get errorMessage => _errorMessage;
 
+  Future<void> checkConnection() async {
+    bool network = await CheckNetwork.check();
+    if (network) {
+      await networkSuccess();
+    } else {
+      await netWorkFailed();
+    }
+  }
+
   Future<void> networkSuccess() async {
-    _isLoading = true;
-    notifyListeners();
     await initialize();
-    _isLoading = false;
-    notifyListeners();
   }
 
   Future<void> netWorkFailed() async {
@@ -56,45 +63,11 @@ class MapState extends ChangeNotifier {
   }
 
   Future<void> initialize() async {
-    await _getCurrentLocation();
+    await getCurrentLocation();
     await loadLocations();
   }
 
-  Future<void> loadLocations() async {
-    if (_isLoading) return;
-    _isLoading = true;
-    notifyListeners();
-
-    try {
-      _locations = await MapLocationApi.getLocations();
-  
-
-      CustomLog.successLog(
-          value: '--------------truyyuyoiujytuhygyhyyu-----------------');
-      _errorMessage = null;
-      CustomLog.successLog(value: 'Loaded ${_locations.length} properties');
-    } catch (e) {
-      _errorMessage = e.toString();
-      _locations = [];
-      CustomLog.errorLog(value: 'Properties load error: $_errorMessage');
-    } finally {
-      _isLoading = false;
-      notifyListeners();
-    }
-  }
-
-  Future<void> centerMapOnUserLocation() async {
-    if (_currentPosition != null) {
-      _mapController.move(_currentPosition!, 14.0);
-    } else {
-      await _getCurrentLocation();
-      if (_currentPosition != null) {
-        _mapController.move(_currentPosition!, 14.0);
-      }
-    }
-  }
-
-  Future<void> _getCurrentLocation() async {
+  Future<void> getCurrentLocation() async {
     try {
       _isLoadingLocation = true;
       notifyListeners();
@@ -104,16 +77,37 @@ class MapState extends ChangeNotifier {
 
       if (lat != null && lng != null) {
         _currentPosition = LatLng(double.parse(lat), double.parse(lng));
-        _mapController.move(_currentPosition!, 14.0);
       } else {
-        _currentPosition =
-            const LatLng(27.7172, 85.3240); // Default to Kathmandu
+        _currentPosition = const LatLng(27.7172, 85.3240);
       }
     } catch (e) {
       CustomLog.errorLog(value: 'Location error: $e');
       _currentPosition = const LatLng(27.7172, 85.3240);
     } finally {
       _isLoadingLocation = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> loadLocations() async {
+    CustomLog.successLog(value: '----bsdkgthtgy---');
+    if (_isLoading) {
+      return;
+    }
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _locations = await MapLocationApi.getLocations();
+      CustomLog.successLog(value: '----truyyuyoiujytuhygyhyyu---');
+      _errorMessage = null;
+      CustomLog.successLog(value: 'Loaded ${_locations.length} properties');
+    } catch (e) {
+      _errorMessage = e.toString();
+      _locations = [];
+      CustomLog.errorLog(value: 'Properties load error: $_errorMessage');
+    } finally {
+      _isLoading = false;
       notifyListeners();
     }
   }
