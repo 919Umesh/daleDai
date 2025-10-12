977b19f0854b624f3db2750f566a3cffdb64dc47
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-12 08:58:03 +0545

    Hide Bottom Bar

--- lib/screen/index/state/index_state.dart
+++ lib/screen/index/state/index_state.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class IndexState extends ChangeNotifier {
   int _currentIndex = 0;
   IconData _fabIcon = Icons.home;
+  bool _isBottomBarVisible = true;
 
   final List<IconData> _icons = [
     Icons.home,
@@ -13,11 +14,36 @@ class IndexState extends ChangeNotifier {
 
   int get currentIndex => _currentIndex;
   IconData get fabIcon => _fabIcon;
+  bool get isBottomBarVisible => _isBottomBarVisible;
   List<IconData> get icons => _icons;
 
   void updateIndex(int index) {
     _currentIndex = index;
     _fabIcon = _icons[index];
+    if (!_isBottomBarVisible) {
+      _isBottomBarVisible = true;
+    }
     notifyListeners();
+  }
+
+  void toggleBottomBarVisibility(bool visible) {
+    if (_isBottomBarVisible != visible) {
+      _isBottomBarVisible = visible;
+      notifyListeners();
+    }
+  }
+
+  void showBottomBar() {
+    if (!_isBottomBarVisible) {
+      _isBottomBarVisible = true;
+      notifyListeners();
+    }
+  }
+
+  void hideBottomBar() {
+    if (_isBottomBarVisible) {
+      _isBottomBarVisible = false;
+      notifyListeners();
+    }
   }
 }
