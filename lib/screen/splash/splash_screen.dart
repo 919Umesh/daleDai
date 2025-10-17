2d94b24480c80590edb62804667374eb646fee70
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-17 22:10:24 +0545

    APK build Splash State

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -7,43 +7,58 @@ import '../../widgets/gerdient_container.dart';
 import '../../widgets/space.dart';
 import 'splash_state.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late final SplashState _splashState; // Store reference here
+  @override
+  void initState() {
+    super.initState();
+    _splashState = Provider.of<SplashState>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _splashState.getContext = context;
+    });
+  }
+
+  @override
+  void dispose() {
+    _splashState.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Provider.of<SplashState>(context, listen: false).getContext = context;
-    return Consumer<SplashState>(
-      builder: (context, state, child) {
-        return GredientContainer(
-          reverseGredient: true,
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            bottomNavigationBar: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                AppDetails.organizeBy,
-                style: subTitleTextStyle.copyWith(color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            body: Center(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                
-                  verticalSpace(10),
-                  Text(
-                    AppDetails.appName,
-                    textAlign: TextAlign.center,
-                    style: cardTextStyleHeader,
-                  ),
-                ],
-              ),
-            ),
+    return GredientContainer(
+      reverseGredient: true,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            AppDetails.organizeBy,
+            style: subTitleTextStyle.copyWith(color: Colors.white),
+            textAlign: TextAlign.center,
           ),
-        );
-      },
+        ),
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              verticalSpace(10),
+              Text(
+                AppDetails.appName,
+                textAlign: TextAlign.center,
+                style: cardTextStyleHeader,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
