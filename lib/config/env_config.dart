1c7af177e20d4804b5466f56b1674bda57fb1bd7
Umesh Shahi <160571077+919Umesh@users.noreply.github.com>
2025-10-18 12:23:29 +0545

    APK build

--- lib/config/env_config.dart
+++ lib/config/env_config.dart
@@ -1,11 +1,5 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class EnvConfig {
-  static String get supabaseUrl => dotenv.get('SUPABASE_URL');
-  static String get supabaseAnonKey => dotenv.get('SUPABASE_ANON_KEY');
-  static String get webClientId => dotenv.get('WEB_CLIENT_ID');
-
-  static Future<void> load() async {
-    await dotenv.load();
-  }
+  static String get supabaseUrl => "https://YOUR_PROJECT_REF.supabase.co";
+  static String get supabaseAnonKey => "YOUR_SUPABASE_ANON_KEY";
+  static String get webClientId => "YOUR_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com";
 }
